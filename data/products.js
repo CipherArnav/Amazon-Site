e60b5efde61de95b53832132c43f9ce44503088b
CipherArnav <arnav14326@gmail.com>
2025-08-21 10:41:49 +0530

    Learned promises and convert code to promises

--- data/products.js
+++ data/products.js
@@ -91,11 +91,37 @@ object3.method();
 
 export let products = [];
 
-    export function loadProducts(fun) {
-      const xhr = new XMLHttpRequest();
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
 
-      xhr.addEventListener('load', () => {
-        products = JSON.parse(xhr.response).map((productDetails) => {
+    console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
+  return promise;
+}
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
       if (productDetails.type === 'clothing') {
         return new Clothing(productDetails);
       }
@@ -106,10 +132,15 @@ export let products = [];
 
     fun();
   });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  });
+
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 }
-loadProducts();
+
 /*
 export const products = [
   {
